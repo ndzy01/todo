edb7679ba3ea1fdbda342de0ad0f11d292e9840d
ndzy01 <2326962137@qq.com>
2023-08-11 05:55:18 +0800

    chore: 禁止缩放

--- src/main.tsx
+++ src/main.tsx
@@ -10,29 +10,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </React.StrictMode>,
 );
-
-document.documentElement.addEventListener(
-  'touchstart',
-  function (event) {
-    if (event.touches.length > 1) {
-      event.preventDefault();
-    }
-  },
-  false,
-);
-
-let lastTouchEnd = 0;
-
-document.documentElement.addEventListener(
-  'touchend',
-  function (event) {
-    const now = Date.now();
-
-    if (now - lastTouchEnd <= 300) {
-      event.preventDefault();
-    }
-
-    lastTouchEnd = now;
-  },
-  false,
-);
