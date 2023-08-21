d68204d8ff10522587fab2212c147d9562752ecb
张一 <zy01066705@alibaba-inc.com>
2023-08-21 18:28:50 +0800

    chore: 优化

--- src/component/Preview.tsx
+++ src/component/Preview.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import Vditor from 'vditor';
 import 'vditor/dist/index.css';
 import { generateUUID } from './utils';
