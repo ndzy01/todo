fcbdad41379f1ef88095194b385d91a853ef429f
ndzy <2326962137@qq.com>
2023-08-30 07:57:50 +0800

    chore: 优化

--- src/component/Preview.tsx
+++ src/component/Preview.tsx
@@ -7,26 +7,9 @@ import { generateUUID } from '../utils';
 const Preview = ({ value }: { value: string }) => {
   const id = generateUUID();
   useLayoutEffect(() => {
-    const VD = new Vditor(`preview-${id}`, {
+    Vditor.preview(document.getElementById(`preview-${id}`) as any, value, {
       cdn: 'https://cdn.jsdelivr.net/npm/vditor@3.9.4',
-      cache: {
-        enable: false,
-      },
-      toolbar: ['fullscreen', 'outline', 'preview'],
-      resize: {
-        enable: true,
-      },
-      minHeight: window.innerHeight * 0.7,
-      value: value || '',
-      outline: {
-        enable: true,
-        position: 'left',
-      },
-      after() {
-        const evt = document.createEvent('Event');
-        evt.initEvent('click', true, true);
-        VD!.vditor!.toolbar!.elements!.preview!.firstElementChild!.dispatchEvent(evt);
-      },
+      mode: 'light',
     });
   }, [value]);
   return <div id={`preview-${id}`} className="ndzy-preview" />;
